ff2241583c3f4e47dcf76633c5346c4363687051
unknown <guoyunzhe@ESG.360ES.CN>
2020-10-28 09:06:23 +0800

    uadd update scheduler

---
+++ utils/update_scheduler.py
@@ -0,0 +1,21 @@
+# update scheduler 
+# generate update plan of every day 
+import time 
+
+class update_scheduler(object):
+    '''
+
+    '''
+    def __init__(self, update_plan = 1, customize_plan = False):
+        
+
+
+        pass
+
+    def is_time_to_fetch(self):
+        # return True or False 
+         
+        pass
+
+    
+    pass
