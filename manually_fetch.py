0101ce07f824c6ec72a1199d265ba3701809043a
GUO Yunzhe <yunzhe.guo@outlook.com>
2020-11-11 18:18:04 +0800

    update summary generator

--- manually_fetch.py
+++ manually_fetch.py
@@ -1,8 +1,8 @@
 #  test code to fetch data from strava 
 try:
-    from token_management import token_manager
-    from activity_management import activity_manager
-    from strava_client import strava_client
+    from utils.token_management import token_manager
+    from utils.activity_management import activity_manager
+    from utils.strava_client import strava_client
 except:
     import sys 
     sys.exit()
@@ -14,7 +14,7 @@ aa = cc.activity_manager
 print('Before')
 print(cc.activity_manager.activity_list)
 
-cc.fetch_activities_last_month(9)
+cc.fetch_activities_last_month(14)
 
 print('After')
 print(cc.activity_manager.activity_list)
