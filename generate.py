546cf002e4218d4fc9cda7c8b39762c3c54b135b
GUO Yunzhe <yunzhe.guo@outlook.com>
2020-11-13 13:13:09 +0800

    update

---
+++ generate.py
@@ -0,0 +1,30 @@
+
+'''
+In [4]: a['distance']
+Out[4]: 17603.7
+
+In [5]: a['elapsed_time']
+Out[5]: 3645
+
+In [6]: a['timezone']
+Out[6]: '(GMT+08:00) Asia/Shanghai'
+
+In [7]: a['calories']
+Out[7]: 339.0
+
+In [8]: a['total_elevation_gain']
+Out[8]: 44.0
+
+In [9]: a['startdate']
+---------------------------------------------------------------------------
+KeyError                                  Traceback (most recent call last)
+<ipython-input-9-92d40b0218f1> in <module>
+----> 1 a['startdate']
+
+KeyError: 'startdate'
+
+In [10]: a['start_date_local']
+Out[10]: '2020-11-11T20:57:40Z'
+
+''' 
+
--- utils/summary_generate.py
+++ utils/summary_generate.py
@@ -17,19 +17,47 @@ class summary_generator(object):
         assert activity_manager is not None
         self.activity_manager = activity_manager # an initialized activity manager 
         self.activity_dict = self.activity_manager.activity_storage 
-
+        self.activity_month = None  # the key of this dict is like '2020-10', '2020-03'
         pass
 
 
     def find_summary_dir_path(self):
-
+        # this dir is the output path 
 
         pass 
 
 
 #  test code  
 ss = summary_generator(activity_manager= activity_manager() )
-example_ID = '4322397441'
+example_ID = '3836008439'
 a = ss.activity_dict[example_ID]
 
+'''
+In [4]: a['distance']
+Out[4]: 17603.7
+
+In [5]: a['elapsed_time']
+Out[5]: 3645
+
+In [6]: a['timezone']
+Out[6]: '(GMT+08:00) Asia/Shanghai'
+
+In [7]: a['calories']
+Out[7]: 339.0
+
+In [8]: a['total_elevation_gain']
+Out[8]: 44.0
+
+In [9]: a['startdate']
+---------------------------------------------------------------------------
+KeyError                                  Traceback (most recent call last)
+<ipython-input-9-92d40b0218f1> in <module>
+----> 1 a['startdate']
+
+KeyError: 'startdate'
+
+In [10]: a['start_date_local']
+Out[10]: '2020-11-11T20:57:40Z'
+
+''' 
 
